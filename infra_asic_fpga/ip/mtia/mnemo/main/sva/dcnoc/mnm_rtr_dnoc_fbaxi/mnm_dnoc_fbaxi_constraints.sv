b7c2104a8d18f8d79a9a0759243b55520d62d020
mohaimen-abdul <mohaimen.abdul@axiomise.com>
2025-04-02 16:49:51 +0100

    Added cnoc credit manager constraints

--- infra_asic_fpga/ip/mtia/mnemo/main/sva/dcnoc/mnm_rtr_dnoc_fbaxi/mnm_dnoc_fbaxi_constraints.sv
+++ infra_asic_fpga/ip/mtia/mnemo/main/sva/dcnoc/mnm_rtr_dnoc_fbaxi/mnm_dnoc_fbaxi_constraints.sv
@@ -15,27 +15,40 @@ module mnm_dnoc_fbaxi_constraints # (
     
 );
 
+  `ifdef ASSERT_OFF
+    `define SV_ASSERT(name, prop) AST_``name : assert property (@(posedge clk) disable iff (!reset_n) prop); 
+  `endif
+
   `include "mnm_dnoc_input_signal_defines.sv"
+
 
 	//------------------------------------------------------------------------------
 	//-- FBAXI Constraints --
 	//------------------------------------------------------------------------------
+	
 	generate
 	
-    // ttl 
+    //------------------------------------------------------------------------------
+    //-- ttl --
+    //------------------------------------------------------------------------------
+
     // Couldn't find d_noc_in.ttl
     // `SV_ASSERT (FVPH_RTR_FV_am_dnoc_ttl_always_at_least_1,     d_noc_in_valid |-> d_noc_in.ttl != '0);
     // `SV_ASSERT (FVPH_RTR_FV_am_dnoc_ttl_always_within_maximum, d_noc_in_valid |-> d_noc_in.ttl <= mnm_pkg::MNM_NOC_TTL_DEFAULT);
     // `SV_ASSERT (FVPH_RTR_FV_am_dnoc_ttl_same_throughout_burst,  d_noc_in_valid && !dnoc_last |=> $stable(d_noc_in.ttl));
 
     localparam TOTAL_NUM_VC_WIDTH = $clog2(NUM_VC + 1);
-    wire [TOTAL_NUM_VC_WIDTH-1:0] dnoc_payload_vc;
-    assign dnoc_payload_vc = (d_noc_in.channel == mnm_pkg::DNOC_CHANNEL_E_READ) ? TOTAL_NUM_VC_WIDTH'('d0)
-                                                                                 :TOTAL_NUM_VC_WIDTH'('d1) + d_noc_in_vc;
+    wire [TOTAL_NUM_VC_WIDTH-1:0] d_noc_payload_vc;
+    assign d_noc_payload_vc = (d_noc_in.channel == mnm_pkg::DNOC_CHANNEL_E_READ) ? TOTAL_NUM_VC_WIDTH'('d0)
+                                                                                  :TOTAL_NUM_VC_WIDTH'('d1) + d_noc_in_vc;
 
-    `SV_ASSERT (FVPH_RTR_FV_am_dnoc_vc_same_throughout_burst, d_noc_in_valid && !d_noc_in_last |=> $stable(dnoc_payload_vc));
+    `SV_ASSERT (FVPH_RTR_FV_am_dnoc_vc_same_throughout_burst, d_noc_in_valid && !d_noc_in_last |=> $stable(d_noc_payload_vc));
 
-		// AXI AW+W channel
+
+    //------------------------------------------------------------------------------
+    //-- AXI AW+W Channel --
+    //------------------------------------------------------------------------------
+
 		`SV_ASSERT (FVPH_RTR_FV_am_dnoc_awvalids_in_burst_are_consecutive, d_noc_in_awvalid && !d_noc_in_wlast |=> d_noc_in_awvalid);
 		`SV_ASSERT (FVPH_RTR_FV_am_dnoc_awlens_in_burst_same_throughout,   d_noc_in_awvalid && !d_noc_in_wlast |=> $stable(d_noc_in_awlen));
 		`SV_ASSERT (FVPH_RTR_FV_am_dnoc_awaddrs_in_burst_same_throughout,  d_noc_in_awvalid && !d_noc_in_wlast |=> $stable(d_noc_in_awaddr));
@@ -72,12 +85,7 @@ module mnm_dnoc_fbaxi_constraints # (
 		
 		// Don't need this yet, will be part of routing
 		// `SV_ASSERT (FVPH_RTR_FV_am_dnoc_block_illegal_ext_awtgids,         !external_illegal_awtgids);
-    // `SV_ASSERT (FVPH_RTR_FV_am_dnoc_block_illegal_aw_z_coord,          valid_aw_z_coord);
-		// `SV_ASSERT (FVPH_RTR_FV_am_dnoc_awsrcid_coords_within_grid, d_noc_in_awvalid |-> (d_noc_in_awsrcid.xcoord >= mnm_pkg::MNM_RTR_GRID_COORD_X_START-1) && (d_noc_in_awsrcid.xcoord <= mnm_pkg::MNM_RTR_GRID_COORD_X_END+1) &&
-    //                                                                                    (d_noc_in_awsrcid.ycoord >= mnm_pkg::MNM_RTR_GRID_COORD_Y_START-1) && (d_noc_in_awsrcid.ycoord <= mnm_pkg::MNM_RTR_GRID_COORD_Y_END+1));
-    // `SV_ASSERT (FVPH_RTR_FV_am_dnoc_awtgtid_coords_within_grid, d_noc_in_awvalid |-> (d_noc_in_awtgtid.xcoord >= mnm_pkg::MNM_RTR_GRID_COORD_X_START-1) && (d_noc_in_awtgtid.xcoord <= mnm_pkg::MNM_RTR_GRID_COORD_X_END+1) &&
-    //                                                                                    (d_noc_in_awtgtid.ycoord >= mnm_pkg::MNM_RTR_GRID_COORD_Y_START-1) && (d_noc_in_awtgtid.ycoord <= mnm_pkg::MNM_RTR_GRID_COORD_Y_END+1));
-
+		
     // Couldn't find d_noc_in.payload.daxi_combo_aw_w.w.strb
     // `SV_ASSERT (FVPH_RTR_FV_am_dnoc_awhost_wstrb_enc_value_is_legal,            d_noc_in_awvalid && awhost |-> wstrb_enc);
     // `SV_ASSERT (FVPH_RTR_FV_am_dnoc_wstrb_beat_offset_in_burst_same_throughout, d_noc_in_awvalid && !d_noc_in_wlast |=> $stable(wstrb_beat_offset));
@@ -89,9 +97,13 @@ module mnm_dnoc_fbaxi_constraints # (
     // Couldn't find d_noc_in_awuser_reduction_type
     // `SV_ASSERT (FVPH_RTR_FV_am_dnoc_awuser_reduction_type_value_is_legal,       d_noc_in_awvalid |-> d_noc_in_awuser_reduction_type inside {4'b0000, 4'b0100, 4'b0101, 4'b0110, 4'b1000,
     // 
-	  // `SV_ASSERT (FVPH_RTR_FV_am_dnoc_awuser_vc_is_legal,                         d_noc_in_awvalid |-> d_noc_in_awuservc < WRITE_VCS);
+	  `SV_ASSERT (FVPH_RTR_FV_am_dnoc_awuser_vc_is_legal,                         d_noc_in_awvalid |-> d_noc_in_awuservc < mnm_pkg::MNM_DNOC_AWW_NUM_VC);
  
-		// AXI R channel
+
+    //------------------------------------------------------------------------------
+    //-- AXI R Channel --
+    //------------------------------------------------------------------------------
+
 		`SV_ASSERT (FVPH_RTR_FV_am_dnoc_rvalids_in_burst_are_consecutive, d_noc_in_rvalid && !d_noc_in_rlast |=> d_noc_in_rvalid);
 		`SV_ASSERT (FVPH_RTR_FV_am_dnoc_rids_in_burst_same_throughout,    d_noc_in_rvalid && !d_noc_in_rlast |=> $stable(d_noc_in_rid));
 		`SV_ASSERT (FVPH_RTR_FV_am_dnoc_rlens_in_burst_same_throughout,   d_noc_in_rvalid && !d_noc_in_rlast |=> $stable(d_noc_in_rlen));
@@ -115,18 +127,8 @@ module mnm_dnoc_fbaxi_constraints # (
 		end
 
 		`SV_ASSERT (FVPH_RTR_FV_am_dnoc_rlast_as_expected,                d_noc_in_rvalid |-> d_noc_in_rlast == (d_noc_in_rdata_items_seen == d_noc_in_rlen));
+    `SV_ASSERT (FVPH_RTR_FV_am_dnoc_ruser_vc_is_legal,                d_noc_in_rvalid |-> d_noc_in_ruservc < mnm_pkg::MNM_DNOC_R_NUM_VC);
 
-		// Don't need this yet, will be part of routing
-		// `SV_ASSERT (FVPH_RTR_FV_am_dnoc_rtgtid_coords_within_grid,        d_noc_in_rvalid |-> (d_noc_in_rtgtid.xcoord >= mnm_pkg::MNM_RTR_GRID_COORD_X_START-1) && (d_noc_in_rtgtid.xcoord <= mnm_pkg::MNM_RTR_GRID_COORD_X_END+1) &&
-		//                                                                                     	(d_noc_in_rtgtid.ycoord >= mnm_pkg::MNM_RTR_GRID_COORD_Y_START-1) && (d_noc_in_rtgtid.ycoord <= mnm_pkg::MNM_RTR_GRID_COORD_Y_END+1));
-		// Couldn't fine d_noc_in.payload.daxi_r.user.cc_lane & d_noc_in.payload.daxi_r.user.cc_dir
-		// `SV_ASSERT (FVPH_RTR_FV_am_dnoc_rcc_opcode_is_legal,              rcc_opcode_is_legal);
-		// `SV_ASSERT (FVPH_RTR_FV_am_dnoc_rcc_lane_is_legal_row,            rcc_lane_is_legal_row);
-		// `SV_ASSERT (FVPH_RTR_FV_am_dnoc_rcc_lane_is_legal_column,         rcc_lane_is_legal_column);
-		
-		
-		// `SV_ASSERT (FVPH_RTR_FV_am_dnoc_awcaches_in_burst_same_throughout, d_noc_in_awvalid && !d_noc_in_wlast |=> $stable(d_noc_in_awcache)); // Couldn't find d_noc_in_awcache
-    // `SV_ASSERT (FVPH_RTR_FV_am_dnoc_awlocks_in_burst_same_throughout,  d_noc_in_awvalid && !d_noc_in_wlast |=> $stable(d_noc_in_awlock));  // Couldn't find d_noc_in_awlock
 	endgenerate
 
 endmodule
