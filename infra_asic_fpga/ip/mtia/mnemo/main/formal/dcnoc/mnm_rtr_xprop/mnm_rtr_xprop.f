64712b3a41246ec752d760aa4b84111e310395be
zifei <zifei.huang@axiomise.com>
2025-04-07 15:45:22 +0100

    fixed xprop

--- infra_asic_fpga/ip/mtia/mnemo/main/formal/dcnoc/mnm_rtr_xprop/mnm_rtr_xprop.f
+++ infra_asic_fpga/ip/mtia/mnemo/main/formal/dcnoc/mnm_rtr_xprop/mnm_rtr_xprop.f
@@ -1,4 +1,4 @@
--f $WORKROOT/min_filelist.f
+-f $WORKROOT/mnm_rtr_ca_dnoc_top_filelist.txt
 
 $INFRA_ASIC_FPGA_TB/ip/mtia/mnemo/main/sva/dcnoc/mnm_rtr_xprop/mnm_rtr_xprop_sva.sv
 
--- infra_asic_fpga/ip/mtia/mnemo/main/sva/dcnoc/mnm_rtr_binds/mnm_xprop_bind.sv
+++ infra_asic_fpga/ip/mtia/mnemo/main/sva/dcnoc/mnm_rtr_binds/mnm_xprop_bind.sv
@@ -1,2 +1,11 @@
-bind mnm_rtr_dc_slice_b mnm_rtr_xprop_sva  #(.STUB(STUB)) 
-                                            u_mnm_rtr_xprop_sva (.*);
+bind mnm_rtr_ca_dnoc_top mnm_rtr_xprop_sva  #(.NUM_LANES(NUM_LANES),
+                                               .NUM_VC(NUM_VC),
+                                               .VCID_W(VCID_W),
+                                               .RX_DEPTH_W(RX_DEPTH_W),
+                                               .NUM_SHRD_CRD_GROUPS(NUM_SHRD_CRD_GROUPS),
+                                               .NUM_RSVD_CRD_GROUPS(NUM_RSVD_CRD_GROUPS),
+                                               .RSVD_CRD_GROUP_ID_W(RSVD_CRD_GROUP_ID_W),
+                                               .SEQN_W(SEQN_W),
+                                               .STUB(STUB),
+                                               .REMOVE_LANE(REMOVE_LANE)) 
+                                               u_mnm_rtr_xprop_sva (.*);
